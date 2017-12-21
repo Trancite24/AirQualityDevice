0d0ada2c759b130ace85f34073c096c4f9e1aaa0
Vindula Jayawardana <vindulamj@gmail.com>
2017-12-21 17:39:55 +0530

    Update error.php

--- error.php
+++ error.php
@@ -4,12 +4,7 @@ $username = "vindula";
 $password = "vindula";
 
 $id = $_GET['id'];
-$longitude = $_GET['longitude'];
-$latitude = $_GET['latitude'];
-$battery = $_GET['battery'];
-$data = $_GET['data'];
-
-$data = json_decode($data);
+$error= $_GET['error'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, "AirQualitydata");
@@ -20,18 +15,13 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully"; echo "<br>";
 
-for ($x = 0; $x < sizeof($data->datapoints); $x++) {
-	$pm10 = $data->datapoints[$x]->pm10;
-	$pm25 = $data->datapoints[$x]->pm25;
-	$timeDate = $data->datapoints[$x]->time;
-	
-   	$sql = "INSERT INTO dataRecords (deviceId, longitude, latitude, battery, timeDate, pm10, pm25) VALUES ($id, $longitude, $latitude, $battery, '$timeDate', $pm10, $pm25)";
+$sql = "INSERT INTO error (deviceId, error) VALUES ($id, $error)";
 
-	if ($conn->query($sql) === TRUE) {
-    		echo "New record created successfully"; echo "<br>";
-	} else {
-    		echo "Error: " . $sql . "<br>" . $conn->error; echo "<br>";
-	}
+if ($conn->query($sql) === TRUE) {
+    	echo "New record created successfully"; echo "<br>";
+} else {
+    	echo "Error: " . $sql . "<br>" . $conn->error; echo "<br>";
+}
 } 
 
 $conn->close();
