6cb5c8ce9b99acbbfdc20a5d53b036060b5e2911
Vindula Jayawardana <vindulamj@gmail.com>
2017-12-21 17:47:21 +0530

    Update error.php

--- error.php
+++ error.php
@@ -15,14 +15,13 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully"; echo "<br>";
 
-$sql = "INSERT INTO error (deviceId, error) VALUES ($id, $error)";
+$sql = "INSERT INTO error (deviceId, error) VALUES ($id, '$error')";
 
 if ($conn->query($sql) === TRUE) {
     	echo "New record created successfully"; echo "<br>";
 } else {
     	echo "Error: " . $sql . "<br>" . $conn->error; echo "<br>";
 }
-} 
 
 $conn->close();
 ?>
